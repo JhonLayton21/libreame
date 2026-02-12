828c0ef9c3dbb59aa05284e669b43216805014ec
Brandon Layton <laytonjhon0@gmail.com>
2026-02-12 16:34:58 -0500

    delete unnecessary files

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
-import { Text, View } from "react-native";
+import Navigation from "./src/navigation";
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>📚 Libreame</Text>
-    </View>
-  );
+  return <Navigation />;
 }
