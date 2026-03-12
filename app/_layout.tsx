c4614689c089a32759288cd5c2e5e9eb9f473cde
Brandon Layton <laytonjhon0@gmail.com>
2026-03-11 19:34:54 -0500

    update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
+import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
